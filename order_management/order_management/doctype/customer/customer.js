bb84fbeaa2f498f1f7e04a3e0d26a94ed6d677e0
Harikrishnan-T <harikrishnan25082003@gmail.com>
2026-09-09 01:06:12 +0530

    feat: create customer doctype

---
+++ order_management/order_management/doctype/customer/customer.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Harikrishnan T and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Customer", {
+// 	refresh(frm) {
+
+// 	},
+// });
